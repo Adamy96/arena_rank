a48fc0366ff41678b852885a2b72aa90637bf1c5
vinicius.adamy.glo <vinicius.adamy@globalhitss.com.br>
2025-03-23 01:47:24 -0300

    Remove yarn.lock

--- src/views/homepage/index.tsx
+++ src/views/homepage/index.tsx
@@ -5,35 +5,6 @@ import styles from "./styles.module.scss";
 const Homepage = async () => {
   const playersData = await fetchPlayers();
 
-  console.log(playersData);
-
-  const topPlayersData = [
-    {
-      icon: "https://ddragon.leagueoflegends.com/cdn/15.6.1/img/profileicon/6091.png",
-      points: 2125,
-      rank: 1,
-      user: "CookiexT#BR1",
-      winrate: 99,
-      wins: 99,
-    },
-    {
-      icon: "https://ddragon.leagueoflegends.com/cdn/15.6.1/img/profileicon/6091.png",
-      points: 1852,
-      rank: 2,
-      user: "Bash#BR1",
-      winrate: 78,
-      wins: 28,
-    },
-    {
-      icon: "https://ddragon.leagueoflegends.com/cdn/15.6.1/img/profileicon/6091.png",
-      points: 1422,
-      rank: 3,
-      user: "Zikardinho#BR3",
-      winrate: 74,
-      wins: 22,
-    },
-  ];
-
   return (
     <div className={styles.homepage}>
       <div className={styles.content}>
