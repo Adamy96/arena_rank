3f4283b65cb1f1fef8187dc40c94300d51c2d365
vinicius.adamy.glo <vinicius.adamy@globalhitss.com.br>
2025-04-03 15:27:43 -0300

    Add more logs

--- src/services/fetchPlayers.ts
+++ src/services/fetchPlayers.ts
@@ -44,37 +44,47 @@ const fetchPlayers = async (): Promise<IPlayer[]> => {
   console.log(`Fetch players called at ${formatter.format(now)}`);
   logger.info(`Fetch players called at ${formatter.format(now)}`);
 
+  const startTime = Date.now();
+
   try {
     const response = await fetch(
       "https://arenaapi.zapto.org:3002/api/Player/Ranking",
       {
-        next: { tags: ["players"], revalidate: 120 },
         signal: AbortSignal.timeout(120000),
       }
     );
 
-    const now2 = new Date();
-    console.log(`FetchPlayers completed at ${formatter.format(now2)}`);
-    const textResponse = await response.text();
+    const endTime = Date.now();
+    const duration = endTime - startTime;
+
+    console.log(
+      `Fetch players completed at ${formatter.format(new Date(endTime))}`
+    );
+    logger.info(
+      `Fetch players completed at ${formatter.format(new Date(endTime))}`
+    );
+    logger.info(`Request duration: ${duration} ms`);
 
     if (!response.ok) {
+      const textResponse = await response.text();
       console.log("Fetch players !response.ok", textResponse);
       logger.error("Fetch players !response.ok", textResponse);
       throw new Error(`Error HTTP: ${response.status}`);
     }
 
-    console.log(
-      `Fetch players response.ok`,
-      JSON.parse(textResponse)?.splice(0, 10)
-    );
-    logger.info(
-      "Fetch players response.ok:",
-      JSON.parse(textResponse)?.splice(0, 10)
-    );
-    return JSON.parse(textResponse);
+    const data = await response.json();
+    console.log(`Fetch players response.ok`, data?.splice(0, 10));
+    logger.info("Fetch players response.ok:", data?.splice(0, 10));
+
+    return data;
   } catch (error) {
+    const endTime = Date.now();
+    const duration = endTime - startTime;
+
+    logger.error(`Request failed after ${duration} ms`);
     console.log("Fetch players catch", error);
     logger.error("Fetch players catch", error);
+
     throw new Error("Failed to fetch players");
   }
 };
